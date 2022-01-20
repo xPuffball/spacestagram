a6a325ec113c81501d1044575d8c88e201031559
Jaemin Han <j4emin.han@gmail.com>
2022-01-19 19:02:03 -0500

    implement header & loading spinner

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -2,6 +2,13 @@
 import react, { useEffect, useState } from 'react'
 import axios from 'axios'
 
+// import material-ui componenets
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import CardHeader from '@material-ui/core/CardHeader';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Avatar from '@material-ui/core/Avatar';
+
 // import componenets
 import PostCard from './PostCard'
 
@@ -9,16 +16,22 @@ const API_KEY = process.env.REACT_APP_NASA_API_KEY
 
 const Feed = () => {
 
+    // state for api response
     const [feed, setFeed] = useState([])
+    
+    // state for loading
+    const [loading, setLoading] = useState(true)
 
     // getFeed => fetch data from NASA API & stores it in state feed
     const getFeed = async () => {
+        setLoading(true)
         axios.get('https://api.nasa.gov/planetary/apod?api_key=' + API_KEY, {
             params: {
                 count: 10
             }
         })
         .then(res => {
+            setLoading(false)
             setFeed(res.data)
         })
         .catch(err => {
@@ -31,11 +44,30 @@ const Feed = () => {
     }, [])
 
     return (
-        <div>
-            {feed.map(post => {
+        <Box style={{display: 'flex', flexDirection: 'column', gap: '2.5rem', alignItems: 'center'}}>
+            <Box style={{margin: '4rem'}}>
+                <CardHeader 
+                    avatar={
+                        <Box style={{fontSize: '50px'}}>
+                            &#127756;
+                        </Box>
+                    }
+                    title={
+                        <Typography variant="h5" fontWeight={'bold'}>
+                            Welcome to Spacestagram!
+                        </Typography>
+                    }
+                    subheader={
+                        <Typography variant="subtitle1">
+                            Browse the coolest pictures from NASA's Astronomy Picture of the Day.
+                        </Typography>
+                    }
+                />
+            </Box>
+            {!loading ? feed.map((post, index) => {
                 return (
                     <PostCard
-                        key={post.date}
+                        key={index}
                         title={post.title}
                         date={post.date}
                         url={post.url}
@@ -44,9 +76,14 @@ const Feed = () => {
                         copyright={post.copyright}
                     />
                 )
-            })}
-            <button onClick={() => console.log(feed)}>Get Feed!</button>           
-        </div>
+            })
+            : 
+            <>
+                <CircularProgress />
+                <Typography variant="caption" fontSize='0.1px' color="textSecondary">Loading your cool (&#128526;) space pictures...</Typography>            
+            </>
+            }      
+        </Box>
     )
 }
 
